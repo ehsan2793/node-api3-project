d8c4ae0441e1e550feab66f43d1ca8b72c808f92
ehsan2793 <ehsan2793@gmail.com>
2021-09-29 11:45:09 -0700

    server setup is complete

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // require your server and launch it
+const server = require('./api/server');
+
+const port = 3001
+
+server.listen(port, () => {
+    console.log(`server listening on http://localhost:${port}`);
+})
