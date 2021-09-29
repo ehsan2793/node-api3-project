7be29b2224a496bdb5adc126d4d6fc35a55b8d89
ehsan2793 <ehsan2793@gmail.com>
2021-09-29 14:46:39 -0700

    logger now severs as global logger

--- api/server.js
+++ api/server.js
@@ -1,14 +1,14 @@
 const express = require('express');
-
+const { logger } = require('./middleware/middleware.js')
 const server = express();
 server.use(express.json());
 
 const userRouter = require('./users/users-router');
-// global middlewares and the user's router need to be connected here
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
+server.use(logger)
 
 server.use('/api/users', userRouter);
 
@@ -16,7 +16,7 @@ server.use('*', handleError);
 
 module.exports = server;
 
-function handleError(err, req, res, next) {
+function handleError(err, req, res, next) { //eslint-disable-line
   res.status(err.status || 500).json({
     message: err.message,
     error: 'oh no we have an error',
